f11ec9c48bd2c5a05ff042fd18726a2ea6c20361
Antonio Coronado <chorao.antonio@gmail.com>
2015-02-11 20:26:09 +0100

    Script cambia fondo

---
+++ .cambiafondo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/antonov/fondo_pantalla/
+while :
+do
+dir='/home/USERNAME/fondo_pantalla/'
+file=`/bin/ls -1 "$dir" | sort --random-sort | head -1`
+#echo "El archivo seleccionado es: $file"
+#echo "cambiado configuración"   
+`gsettings set org.gnome.desktop.background picture-uri "file:////home/USERNAME/fondo_pantalla/$file"`
+#echo " Cambiado!!"
+sleep 100
+done
+
